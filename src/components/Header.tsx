c0aa0448acc2ae1ab87437824703b2183044a3f3
younggyulee <baayoo93@naver.com>
2022-01-28 18:02:03 +0900

    Fix: 헤더 css수정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,38 +6,57 @@ import styled from "styled-components";
 const Header = () => {
   return (
     <>
-      <HeaderDiv>
-        <HeadDiv>
-          <StyledLink to={`/`}>
-            <NameDiv>we</NameDiv>
-          </StyledLink>
-          <StyledLink to={`/basket`}>
-            <div>로그인</div>
-          </StyledLink>
-        </HeadDiv>
-      </HeaderDiv>
+      <Container>
+        <Head>
+          <HeadContainer>
+            <StyledLink to={`/`}>
+              <NameText>We Study</NameText>
+            </StyledLink>
+            <StyledLink to={`/basket`}>
+              <LoginText>로그인</LoginText>
+            </StyledLink>
+          </HeadContainer>
+        </Head>
+      </Container>
     </>
   );
 };
-const NameDiv = styled.div`
+
+const HeadContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const LoginText = styled.div`
+  color: #fff;
+  font-size: 22px;
+`;
+const NameText = styled.div`
   font-size: 40px;
   font-weight: bold;
+  color: #fff;
 `;
-const HeaderDiv = styled.header`
+const Container = styled.header`
   width: 100%;
-  height: 70px;
+  height: 60px;
   top: 0;
   left: 0;
   z-index: 1000;
   display: flex;
+  background-color: #0c151c;
+  box-shadow: 0px 2px 6px 0px #00000017;
+  position: fixed;
 `;
 
-const HeadDiv = styled.div`
-  width: 100%;
+const Head = styled.div`
+  width: 80%;
   heigth: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  margin: 0 auto;
+  padding: 0rem 1rem;
+  overflow: hidden;
 `;
 
 const StyledLink = styled(Link)`

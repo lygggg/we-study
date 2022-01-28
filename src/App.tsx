17bdd365b2ade16964e1948f449b87b8e6e957c4
younggyulee <baayoo93@naver.com>
2022-01-28 15:15:15 +0900

    Add: 퀴즈페이지 생성후 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import QuestionPage from "./pages/QuestionPage";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<QuestionPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/QuestionPage.tsx
@@ -0,0 +1,5 @@
+function QuestionPage() {
+  return <div>퀴즈 페이지</div>;
+}
+
+export default QuestionPage;

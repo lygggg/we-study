39c980d47d5e3dd34ad40f3715181bdd2d0851b4
younggyulee <baayoo93@naver.com>
2022-01-30 19:57:31 +0900

    Add: 질문 페이지 생성

--- src/pages/QuestionPage.tsx
+++ src/pages/QuestionPage.tsx
@@ -1,5 +1,18 @@
-function QuestionPage() {
-  return <div>퀴즈 페이지</div>;
-}
+import styled from "styled-components";
+import QuestionList from "../components/QuestionList";
+
+const QuestionPage = () => {
+  return (
+    <>
+      <Container>
+        <QuestionList />
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  margin-top: 60px;
+`;
 
 export default QuestionPage;

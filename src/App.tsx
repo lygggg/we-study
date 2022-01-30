4829244fa3796819d591d71098eb0ee81633f8b6
younggyulee <baayoo93@naver.com>
2022-01-30 19:18:50 +0900

    Add: 회원가입 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import QuestionPage from "./pages/QuestionPage";
 import LoginPage from "./pages/LoginPage";
 import Header from "./components/Header";
+import SignUpPage from "./pages/SignUpPage";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<QuestionPage />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignUpPage />} />
           </Routes>
         </Container>
       </BrowserRouter>

cd92cb08b6940eb3a42650dcbe3ac2acfb506ddb
younggyulee <baayoo93@naver.com>
2022-02-03 23:19:45 +0900

    Add: firebase 연동

---
+++ client/src/firebase/Firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getAuth, // authentication 설정
+  signInWithPopup, //google 로그인을 팝업창에 띄우기 위해
+  GoogleAuthProvider, //google login 기능
+  signInWithEmailAndPassword, // email 로그인
+  createUserWithEmailAndPassword, //email 회원가입
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APPID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+

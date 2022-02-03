70511a63af1b79c5860e63fb167f27b48e9f0678
younggyulee <baayoo93@naver.com>
2022-02-03 15:01:30 +0900

    Add: express.json코드

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const port = 3000;
 require("./models");
 require("dotenv").config();
 app.use("/", require("./routes"));
+app.use(express.json());
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);

1648cae772cdfaaaeabf2791f90565caa9fff7d7
younggyulee <baayoo93@naver.com>
2022-02-03 15:43:35 +0900

    Feat: 메뉴 링크 설정

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,19 +1,37 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Menu = () => {
   return (
     <>
       <Container>
-        <Text>운영체제</Text>
-        <Text>네트워크</Text>
-        <Text>자바스크립트</Text>
-        <Text>React</Text>
-        <Text>자료구조</Text>
-        <Text>프론트엔드 질문</Text>
+        <Text>
+          <StyledLink to="operating">운영체제</StyledLink>
+        </Text>
+        <Text>
+          <StyledLink to="network">네트워크</StyledLink>
+        </Text>
+        <Text>
+          <StyledLink to="Javascript">자바스크립트</StyledLink>
+        </Text>
+        <Text>
+          <StyledLink to="react">React</StyledLink>
+        </Text>
+        <Text>
+          <StyledLink to="datastructure">자료구조</StyledLink>
+        </Text>
+        <Text>
+          <StyledLink to="frontend">프론트엔드 질문</StyledLink>
+        </Text>
       </Container>
     </>
   );
 };
+
+const StyledLink = styled(Link)`
+  text-decoration-line: none;
+  color: #000000;
+`;
 
 const Text = styled.div`
   font-size: 20px;

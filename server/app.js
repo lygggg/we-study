5e771aba0e36a717f41736aff4cac55dcc2fa887
younggyulee <baayoo93@naver.com>
2022-02-03 17:35:30 +0900

    Add: cors 설정

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const app = express();
 
 const port = 3000;
 
 require("./models");
 require("dotenv").config();
-app.use("/", require("./routes"));
 app.use(express.json());
+app.use(cors({ origin: process.env.CORS_ORIGIN_DEV, credential: true }));
+app.use("/", require("./routes"));
 
 app.listen(port, () => {
   console.log(`server is running on ${port}`);

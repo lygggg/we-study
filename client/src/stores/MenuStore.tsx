3bb7abd214939cae9bd0af75dd94e31056c79770
younggyulee <baayoo93@naver.com>
2022-02-03 17:30:08 +0900

    Modify: 메뉴를 저장하는 스토어로 변경

---
+++ client/src/stores/MenuStore.tsx
@@ -0,0 +1,16 @@
+const MenuStore = {
+  categories: [
+    "운영체제",
+    "네트워크",
+    "자바스크립트",
+    "React",
+    "자료구조",
+    "프론트엔드 질문",
+  ],
+
+  getCategories() {
+    return this.categories;
+  },
+};
+
+export default MenuStore;

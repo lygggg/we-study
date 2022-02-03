91a83a3798bc7c49fd8618aaeb284d24dda9a957
younggyulee <baayoo93@naver.com>
2022-02-03 17:33:05 +0900

    Refactor: 메뉴 컴포넌트 출력

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -1,28 +1,18 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import MenuStore from "../stores/MenuStore";
+import Category from "./Category";
 
 const Menu = () => {
+  const categories = MenuStore.getCategories();
   return (
     <>
       <Container>
-        <Text>
-          <StyledLink to="operating">운영체제</StyledLink>
-        </Text>
-        <Text>
-          <StyledLink to="network">네트워크</StyledLink>
-        </Text>
-        <Text>
-          <StyledLink to="Javascript">자바스크립트</StyledLink>
-        </Text>
-        <Text>
-          <StyledLink to="react">React</StyledLink>
-        </Text>
-        <Text>
-          <StyledLink to="datastructure">자료구조</StyledLink>
-        </Text>
-        <Text>
-          <StyledLink to="frontend">프론트엔드 질문</StyledLink>
-        </Text>
+        {categories.map((category, index) => (
+          <Text key={index}>
+            <Category categoryName={category} categoryId={index} />
+          </Text>
+        ))}
       </Container>
     </>
   );

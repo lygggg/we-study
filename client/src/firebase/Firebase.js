4b31117a6abb024a4c1aab30e062deccc4628371
younggyulee <baayoo93@naver.com>
2022-02-03 23:22:26 +0900

    Feat: Firebase 구글 로그인 가져오기

--- client/src/firebase/Firebase.js
+++ client/src/firebase/Firebase.js
@@ -30,3 +30,25 @@ export const loginEmail = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password);
 };
 
+const provider = new GoogleAuthProvider();
+export const loginGoogle = () => {
+  return signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // ...
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
+};

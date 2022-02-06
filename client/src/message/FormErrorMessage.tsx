a7c424542d30e9e6632efed4766fbb35b7d97a9e
younggyulee <baayoo93@naver.com>
2022-02-07 05:04:05 +0900

    Add: 회원가입 에러메세지 컴포넌트 생성

---
+++ client/src/message/FormErrorMessage.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+type FormErrorMessage = {
+  errorMessage: string;
+};
+
+const StyledFormErrorMessage = styled.div`
+  color: #ff4848;
+  text-align-last: center;
+  font-size: 17px;
+  margin-left: 50px;
+`;
+
+const FormErrorMessage = ({ errorMessage }: FormErrorMessage) => {
+  return <StyledFormErrorMessage>{errorMessage}</StyledFormErrorMessage>;
+};
+
+export default FormErrorMessage;

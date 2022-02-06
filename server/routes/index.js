64081fd35f4499d10409688580f00f15ee18ed93
younggyulee <baayoo93@naver.com>
2022-02-07 05:06:38 +0900

    Add: auth router 생성

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 console.log("라우트");
-// router.use("/auth", require("./auth.js"));
+router.use("/auth", require("./auth"));
 router.use("/quiz", require("./quiz"));
 
 module.exports = router;

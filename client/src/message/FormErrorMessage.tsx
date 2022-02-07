093983d488da093320506500914ceb00e49c24ef
younggyulee <baayoo93@naver.com>
2022-02-07 18:16:28 +0900

    Modify: 에러메시지 css 마진 삭제

--- client/src/message/FormErrorMessage.tsx
+++ client/src/message/FormErrorMessage.tsx
@@ -9,7 +9,6 @@ const StyledFormErrorMessage = styled.div`
   color: #ff4848;
   text-align-last: center;
   font-size: 17px;
-  margin-left: 50px;
 `;
 
 const FormErrorMessage = ({ errorMessage }: FormErrorMessage) => {

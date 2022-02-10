9448382460b716c250bb874d2410aacc420f52d4
younggyulee <baayoo93@naver.com>
2022-02-10 11:08:27 +0900

    Feat: App에서 로그인 상태전달
    App에서 만약 유저정보가 변경이 되었다면 유저정보를 불러와서 userState에 저장

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,51 @@
+import { useEffect } from "react";
+import axios from "axios";
+import { useSetRecoilState } from "recoil";
+import { getAuth } from "firebase/auth";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import styled from "styled-components";
+import { userState } from "./atom/user";
 import HomePage from "./pages/HomePage";
 import QuestionPage from "./pages/QuestionPage";
 import LoginPage from "./pages/LoginPage";
 import Header from "./components/Header";
 import SignUpPage from "./pages/SignUpPage";
+import { getUser } from "./services/Login";
 import "./App.css";
 
 function App() {
+  const setUser = useSetRecoilState(userState);
+
+  const saveUserData = async (userData) => {
+    const data = await getUser(userData.email);
+    setUser(data.user);
+  };
+
+  useEffect(() => {
+    getAuth().onAuthStateChanged(async (user) => {
+      if (!user) return;
+
+      // axios.defaults.headers.common["Authorization"] =
+      //   "Bearer " + (await user.getIdToken());
+      saveUserData(user);
+    });
+  }, []);
+
   return (
-    <>
-      <BrowserRouter>
-        <Container>
-          <Header />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-            <Route
-              path="/categories/:categoryId/page/:pageNum"
-              element={<QuestionPage />}
-            />
-          </Routes>
-        </Container>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route
+            path="/categories/:categoryId/page/:pageNum"
+            element={<QuestionPage />}
+          />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
 }
 

efa76556d42cf262686dec0f1d4d81b6bc269caf
younggyulee <baayoo93@naver.com>
2022-02-10 10:55:52 +0900

    Modify: 퀴즈 스키마 수정
    populate를 할 수 있도록 타입을 ObjectId로 변경

--- server/models/quiz.js
+++ server/models/quiz.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
+
 const quizSchema = new mongoose.Schema({
   category: { type: Number, required: true },
   quizText: { type: String, required: true },
-  userEmail: { type: String, required: true },
+  user: [{ type: mongoose.ObjectId, ref: "users", required: true }],
+  // https://mongoosejs.com/docs/populate.html
   answerText: { type: String, required: true },
 });
 

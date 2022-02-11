38c5ce9280fd6e192c4723deb72508afbf6fa371
younggyulee <baayoo93@naver.com>
2022-02-11 23:12:02 +0900

    Feat:  검색 데이터베이스에 퀴즈생성

--- server/repositorys/quiz.repository.js
+++ server/repositorys/quiz.repository.js
@@ -1,4 +1,12 @@
 const Quiz = require("../models/quiz");
+const algoliasearch = require("algoliasearch");
+
+const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
+const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
+const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME;
+
+const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
+const index = client.initIndex(ALGOLIA_INDEX_NAME);
 
 module.exports = {
   async getQuizAll(category) {
@@ -12,12 +20,16 @@ module.exports = {
   },
 
   async createQuiz(quizText, answerText, category, id) {
-    const quiz = await Quiz.create({
-      category,
-      quizText,
+    const data = {
+      category: category,
+      quizText: quizText,
       user: id,
-      answerText,
-    });
+      answerText: answerText,
+    };
+
+    const quiz = await Quiz.create(data);
+    await index.saveObjects([data], { autoGenerateObjectIDIfNotExist: true });
+    console.log(quiz); // 이거 데이터 가져와서 populate해야하나..?
     return quiz;
   },
 };

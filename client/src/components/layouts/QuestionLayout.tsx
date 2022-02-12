1fe1781c425771fad4f33ad03048a4ebe2989c9f
younggyulee <baayoo93@naver.com>
2022-02-12 18:37:21 +0900

    Feat: 빈 검샐결과 화면 출력
    데이터가 비어있을때 조건부 렌더링

--- client/src/components/layouts/QuestionLayout.tsx
+++ client/src/components/layouts/QuestionLayout.tsx
@@ -2,33 +2,55 @@ import React, { useState } from "react";
 import styled from "styled-components";
 import Question from "../items/QuestionItem";
 import SolveModal from "../modals/SolveModal";
+import Info from "../../components/Info";
 
 function QuestionLayout({ questionList }) {
   const [showModal, setShowModal] = useState(false);
-
   const onClickModal = () => {
     setShowModal(true);
   };
   return (
     <>
       <Container>
-        {questionList.map((question) => (
-          <Inner key={question._id} onClick={onClickModal}>
-            <QuestionCotainer>
-              <Question question={question}></Question>
-            </QuestionCotainer>
+        <QuestionContainer>
+          {questionList.length > 0 ? (
+            <>
+              {questionList.map((question) => (
+                <Inner key={question._id} onClick={onClickModal}>
+                  <QuestionCotainer>
+                    <Question question={question}></Question>
+                  </QuestionCotainer>
 
-            <SolveModal
-              open={showModal}
-              question={question}
-              onClose={() => setShowModal(false)}
-            />
-          </Inner>
-        ))}
+                  <SolveModal
+                    open={showModal}
+                    question={question}
+                    onClose={() => setShowModal(false)}
+                  />
+                </Inner>
+              ))}
+            </>
+          ) : (
+            <Empty>아무런 값도 찾지 못했습니다.</Empty>
+          )}
+        </QuestionContainer>
+        <Info />
       </Container>
     </>
   );
 }
+
+const QuestionContainer = styled.div``;
+const Empty = styled.div`
+  height: 400px;
+  width: 800px;
+  margin-top: 20px;
+  border: 0.0625rem solid #d7e2eb;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 25px;
+`;
 
 const QuestionCotainer = styled.div`
   display: flex;
@@ -36,10 +58,12 @@ const QuestionCotainer = styled.div`
 const Container = styled.div`
   margin-top: 60px;
   box-sizing: border-box;
+  justify-content: space-between;
+  display: flex;
 `;
 
 const Inner = styled.div`
-  height: 100%;
+  width: 800px;
   margin-top: 20px;
   border: 0.0625rem solid #d7e2eb;
   padding: 1rem;

f11ee8196c79794af17065b81232bb5f1bad945e
younggyulee <baayoo93@naver.com>
2022-02-18 02:28:43 +0900

    Add: quiz스키마 타입추가
    quiz 스키마에 img타입 추가

--- server/models/quiz.js
+++ server/models/quiz.js
@@ -4,8 +4,8 @@ const quizSchema = new mongoose.Schema({
   category: { type: Number, required: true },
   quizText: { type: String, required: true },
   user: [{ type: mongoose.ObjectId, ref: "users", required: true }],
-  // https://mongoosejs.com/docs/populate.html
   answerText: { type: String, required: true },
+  img: { type: String, required: true },
 });
 
 module.exports = mongoose.model("quizzes", quizSchema);

d033b09f0515b78e050743258518885c21a4087c
younggyulee <baayoo93@naver.com>
2022-02-21 14:03:18 +0900

    Refactor: 퀴즈 리스트 가져오는 함수 hook에 정리

--- client/src/pages/QuestionPage.tsx
+++ client/src/pages/QuestionPage.tsx
@@ -1,58 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
 import styled from "styled-components";
-import { useRecoilValue } from "recoil";
-import { questionState } from "../atom/question";
-import { getQuestion } from "../services/Question";
-import { useMe } from "../hook/useMe";
-import MenuStore from "../stores/MenuStore";
 import QuestionLayout from "../components/layouts/QuestionLayout";
-import Modal from "../components/modals/AddModal";
 import GetError from "../errorComponent/GetError";
+import { useQuestions } from "../hook/useQuestions";
 
 const QuestionPage = () => {
-  const user = useMe();
   const [error, setError] = useState();
-  const value = useRecoilValue(questionState);
-  const { categoryId } = useParams();
-  const [questionList, setQuestionList] = useState([]);
-
-  const fetchQuestions = async () => {
-    try {
-      const data = await getQuestion({
-        category: categoryId,
-      });
-      setQuestionList(data.quizs);
-      setError(null);
-    } catch (e) {
-      setError(e);
-    }
-  };
-
-  useEffect(() => {
-    fetchQuestions();
-  }, [categoryId]);
-
-  useEffect(() => {
-    fetchQuestions();
-  }, [value]);
+  const questionList = useQuestions({
+    onError: setError,
+  });
 
   return (
     <>
       <Container>
         {!!error ? (
-          <GetError fetchQuestions={fetchQuestions} />
+          <GetError />
         ) : (
           <>
-            {!!Object.keys(user).length ? (
-              <Modal
-                category={MenuStore.categories[categoryId]}
-                categoryId={categoryId}
-              />
-            ) : (
-              <></>
-            )}
-
             <QuestionLayout questionList={questionList} />
           </>
         )}

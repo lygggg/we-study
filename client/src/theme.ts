14a828757a706fefc7e675493cb8f28a88439afd
younggyulee <baayoo93@naver.com>
2022-02-22 17:27:34 +0900

    Add: 다크테마, 밝은테마 css설정

---
+++ client/src/theme.ts
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  body: "#EAEAEA",
+  text: "#353535",
+  mainColor: "#7038d4",
+  toggleBackground: "#F6F6F6",
+  header: "#4C4C4C",
+};
+
+export const darkTheme = {
+  body: "#3A3A3A",
+  text: "#fafafa",
+  mainColor: "#7038d4",
+  toggleBackground: "#333333",
+  header: "#EAEAEA",
+};
+
+export type Theme = typeof lightTheme;

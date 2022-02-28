b40629f41896aa31ea3439e4a1674dae1fa0cda3
younggyulee <baayoo93@naver.com>
2022-03-01 01:30:38 +0900

    Refactor: firebase.js 안쓰는 코드 삭제

--- client/src/firebase/Firebase.ts
+++ client/src/firebase/Firebase.ts
@@ -19,7 +19,6 @@ const firebaseConfig: any = {
   appId: import.meta.env.VITE_APP_FIREBASE_APPID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const provider = new GoogleAuthProvider();
@@ -35,22 +34,11 @@ export const loginEmail = (email: string, password: string) => {
 export const loginGoogle = () => {
   return signInWithPopup(auth, provider)
     .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
       const credential = GoogleAuthProvider.credentialFromResult(result);
       const token = credential?.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      // ...
     })
     .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
       const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
     });
 };
 

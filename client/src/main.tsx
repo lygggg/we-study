0edb14aa1acb2243d12616bd66f9f5820351c7f2
younggyulee <baayoo93@naver.com>
2022-02-28 22:16:56 +0900

    Add: main.tsx에 스켈레톤 css 생성

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
+
+import "react-loading-skeleton/dist/skeleton.css";
+
 import "./index.css";
 import App from "./App";
 

7f71b867d577b8b54ee88b7754aefbe0cad059b5
younggyulee <baayoo93@naver.com>
2022-02-28 22:13:52 +0900

    Add: 테마 css 타입 설정

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,10 +1,18 @@
+export interface ITheme {
+  body: string;
+  text: string;
+  mainColor: string;
+  toggleBackground: string;
+  header: string;
+}
+
 export const lightTheme = {
   body: "#FFFFFF",
   text: "#353535",
   mainColor: "#7038d4",
   toggleBackground: "#F6F6F6",
   header: "#4C4C4C",
-};
+} as ITheme;
 
 export const darkTheme = {
   body: "#282c35",
@@ -12,6 +20,6 @@ export const darkTheme = {
   mainColor: "#7038d4",
   toggleBackground: "#333333",
   header: "#3A3A3A",
-};
+} as ITheme;
 
 export type Theme = typeof lightTheme;

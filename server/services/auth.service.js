da89af54d127bbd66c9a709458cae2fe290bd151
younggyulee <baayoo93@naver.com>
2022-03-08 20:47:57 +0900

    Docs: auth.service 주석처리

--- server/services/auth.service.js
+++ server/services/auth.service.js
@@ -7,6 +7,7 @@ module.exports = {
     const { name, email, userId } = req.body;
     const user = await authRepo.createUser(userId, name, email);
     res.status(200).json({ user, message: "success add new user" });
+    return;
   },
 
   async loginUser(req, res) {
@@ -16,7 +17,7 @@ module.exports = {
     const quizCart = await quizCartRepo.getCart(userId);
     res.status(200).json({
       user: {
-        ...user._doc,
+        ...user?._doc, // 몽구스로 가져온 객체는 보호된 필드라 _doc를 사용하면 오브젝트 안을 볼 수 있다.
         myQuizCount: myQuiz.length,
         cartCount: quizCart.length,
       },

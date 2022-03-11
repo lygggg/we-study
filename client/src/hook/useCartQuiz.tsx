9e70380e3b824d664ef9a30cfd6a2d171f7876ed
younggyulee <baayoo93@naver.com>
2022-03-11 13:20:24 +0900

    Feat: 퀴즈 좋아요, 좋아요 취소 로직 hook 폴더에 정리

--- client/src/hook/useCartQuiz.tsx
+++ client/src/hook/useCartQuiz.tsx
@@ -4,7 +4,8 @@ import { Quiz } from "../models/quiz";
 import { useMe } from "./useMe";
 import { removeQuizCart } from "../services/QuizCart";
 import { useSetRecoilState, useRecoilValue } from "recoil";
-import { cartQuizState } from "../recoilState/cartQuiz";
+import { likeQuizState } from "../recoilState/like";
+import { createQuizCart } from "../services/QuizCart";
 
 interface UseQuizsOptions {
   onError?: (error: any) => void;
@@ -12,7 +13,7 @@ interface UseQuizsOptions {
 
 export const useCartQuizList = ({ onError }: UseQuizsOptions = {}) => {
   const [quizList, setQuizList] = useState<Quiz[] | undefined>();
-  const cartQuiz = useRecoilValue(cartQuizState);
+  const cartQuiz = useRecoilValue(likeQuizState);
   const user = useMe();
 
   const fetchQuizs = async () => {
@@ -34,11 +35,27 @@ export const useCartQuizList = ({ onError }: UseQuizsOptions = {}) => {
 };
 
 export const useRemoveCartQuiz = (quizId: string) => {
-  const setCartQuiz = useSetRecoilState<string>(cartQuizState);
+  const setLikeQuiz = useSetRecoilState<string>(likeQuizState);
+
   const removeQuiz = async () => {
     await removeQuizCart(quizId);
-    setCartQuiz(quizId);
+    setLikeQuiz(quizId);
   };
 
   return removeQuiz;
+};
+
+export const useCreateQuizCart = (quizId) => {
+  const setLikeQuiz = useSetRecoilState<string>(likeQuizState);
+
+  const likeQuiz = async () => {
+    try {
+      await createQuizCart({ quizId: quizId });
+      setLikeQuiz(quizId);
+    } catch (e) {
+      alert("이미 소장한 퀴즈입니다.");
+    }
+  };
+
+  return likeQuiz;
 };

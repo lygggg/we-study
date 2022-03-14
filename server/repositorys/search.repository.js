af27a9b12b979e9fe856b8446dca1f6866368a2d
younggyulee <baayoo93@naver.com>
2022-03-14 16:45:16 +0900

    Fix: 검색 퀴즈는 좋아요가 안되던 문제 해결

--- server/repositorys/search.repository.js
+++ server/repositorys/search.repository.js
@@ -1,4 +1,5 @@
 const algoliasearch = require("algoliasearch");
+const quiz = require("../models/quiz");
 
 const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID;
 const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY;
@@ -9,35 +10,24 @@ const index = client.initIndex(ALGOLIA_INDEX_NAME);
 
 module.exports = {
   async getSearchQuiz(query, userId) {
-    const quizs = await index.search(query);
+    const { hits } = await index.search(query);
+    const quizs = await quiz
+      .find()
+      .in(
+        "_id",
+        hits.map((x) => x._id),
+      )
+      .populate("user");
+
     if (userId) {
-      quizs.hits.forEach((quiz) => {
+      quizs.forEach((quiz) => {
         if (quiz.like_users.indexOf(userId) !== -1) {
           quiz.like = true;
-          quiz.likeCount = Number(quiz.like_users.length);
+          quiz.likeCount = quiz.like_users.length;
         }
       });
     }
+
     return quizs;
-  },
-
-  async likeQuiz(quizId, userId) {
-    await index.partialUpdateObject({
-      like_users: {
-        _operation: "AddUnique",
-        value: userId,
-      },
-      objectID: quizId,
-    });
-  },
-
-  async unLikeQuiz(quizId, userId) {
-    await index.partialUpdateObject({
-      like_users: {
-        _operation: "Remove",
-        value: userId,
-      },
-      objectID: quizId,
-    });
   },
 };

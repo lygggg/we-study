bbc5296cc6b9ad0e9242a67c66492d839ef0c192
younggyulee <baayoo93@naver.com>
2022-03-14 16:58:39 +0900

    Modify: quill css, placeholer 설정

--- client/src/components/modals/Editor.tsx
+++ client/src/components/modals/Editor.tsx
@@ -1,21 +1,25 @@
+import { size } from "cypress/types/lodash";
 import * as ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import styled from "styled-components";
 
 interface Editor {
+  className?: string;
   value: string;
   onChange: (x: string) => void;
 }
 
-const Editor = ({ value, onChange }: Editor) => {
+const Editor = ({ className, value, onChange }: Editor) => {
   return (
     <>
       <Container>
         <ReactQuill
-          style={{ height: 200, width: 800, color: "#FFFFFF" }}
+          className={className}
+          style={{ height: 200, width: 560, color: "#FFFFFF", fontSize: 20 }}
           theme="snow"
           value={value}
           onChange={onChange}
+          placeholder="정답을 입력해주세요"
         />
       </Container>
     </>

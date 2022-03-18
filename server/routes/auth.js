d819a8e7daed8b41feabddbd8301a11e03bd9edc
younggyulee <baayoo93@naver.com>
2022-03-18 15:33:40 +0900

    Modify: 유저정보 가져오는 메소드 get으로 변경

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,6 +7,6 @@ const errorCatcher = require("../middlewares/errorCatcher.js");
 const authService = require("../services/auth.service.js");
 
 router.post("/signup", authChecker, errorCatcher(authService.signUpUser));
-router.post("/me", authChecker, errorCatcher(authService.loginUser));
+router.get("/me", authChecker, errorCatcher(authService.getUser));
 
 module.exports = router;

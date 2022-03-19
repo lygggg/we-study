57f312d1e4427245df1abe3f3b99934022817625
younggyulee <baayoo93@naver.com>
2022-03-19 21:52:24 +0900

    Add: 페이지네이션 컴포넌트생성

---
+++ client/src/components/items/Pagination.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+const PageDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Button = styled.button`
+  width: 40px;
+  height: 34px;
+  line-height: 34px;
+  font-size: 12px;
+  font-weight: bold;
+  border: 1px solid #6c6c6c;
+  color: #6c6c6c;
+  background: #fff;
+  display: inline-block;
+  padding: 0 5px;
+  text-align: center;
+  margin-left: 1;
+  margin: 5px;
+`;
+
+function Pagination({ total, pageSize, onPageChange }) {
+  const pagesCount = Math.ceil(total / pageSize);
+  const pages = [];
+
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
+
+  return (
+    <PageDiv>
+      <Button>이전</Button>
+      {!pagesCount && <Button>{1}</Button>}
+      {pages.map((e) => (
+        <Button onClick={() => onPageChange(e)} key={e}>
+          {e}
+        </Button>
+      ))}
+      <Button>다음</Button>
+    </PageDiv>
+  );
+}
+
+export default Pagination;

7dd833b20090859ad95145440e6fbbd82c7c3d6b
younggyulee <baayoo93@naver.com>
2022-03-28 23:49:09 +0900

    Remove: 기존 퀴즈 페이지네이션 코드 삭제

--- client/src/services/Quiz.tsx
+++ client/src/services/Quiz.tsx
@@ -2,26 +2,11 @@ import {
   getQuizs as apiGetQuizs,
   sliceQuizs as apiSliceQuizs,
   getUserAddQuizs as apiGetUserAddQuizs,
-  getSliceAddQuizs as apiSliceAddQuizs,
   createQuiz as apiCreateQuiz,
   removeQuiz as apiRemoveLikeQuiz,
   updateQuiz as apiUpdateLikeQuiz,
 } from "../apis/Quiz";
 
-export const getQuizs = async ({ category }) => {
-  return await apiGetQuizs({ category });
-};
-
-export const getSliceQuizs = async ({ category, pageNumber }) => {
-  return await apiSliceQuizs({ category, pageNumber });
-};
-
-export const getUserAddQuizs = async () => {
-  return await apiGetUserAddQuizs();
-};
-
-export const getSliceAddQuizs = async (pageNumber) => {
-  return await apiSliceAddQuizs(pageNumber);
 export const getQuizs = async ({ category, page }) => {
   return await apiGetQuizs({ category, page });
 };
